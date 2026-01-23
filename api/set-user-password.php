ff7108d09deaf5e920bf33eb1fca895801904db9
mightyblade <bigpetey22@hotmail.com>
2026-01-23 13:13:54 -0700

    Switch driver login to usernames

---
+++ api/set-user-password.php
@@ -0,0 +1,12 @@
+<?php
+require __DIR__ . '/bootstrap.php';
+
+require_role('admin');
+$input = json_input();
+$password = trim($input['password'] ?? '');
+if ($password === '') {
+    respond(['error' => 'Password required'], 400);
+}
+
+set_setting($pdo, 'user_password_hash', password_hash($password, PASSWORD_DEFAULT));
+respond(['ok' => true]);
